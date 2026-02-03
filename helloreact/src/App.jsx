7503fe37f3a010bc6a16f96f157418465bb42e7e
vicentematheus15 <vicentematheus1507@gmail.com>
2026-02-02 21:55:19 -0300

    add Cabecalho component and styles, update App component to include header

--- helloreact/src/App.jsx
+++ helloreact/src/App.jsx
@@ -1,10 +1,12 @@
 
 import './App.css'
+import Cabecalho from './components/Cabecalho'
 
 function App() {
 
   return (
     <>
+      <Cabecalho />
       <h1>Hello React</h1>
     </>
   )
--- helloreact/src/components/Cabecalho.jsx
+++ helloreact/src/components/Cabecalho.jsx
@@ -1,4 +1,4 @@
-
+import './Cabecalho.css'
 function Cabecalho() {
 
     return (
