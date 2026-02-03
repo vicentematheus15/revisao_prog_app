89687cd4de8d2ce9b3cb2332584a1ae9fa7cbd9e
vicentematheus15 <vicentematheus1507@gmail.com>
2026-02-03 19:55:46 -0300

    add button to App component for handling click events

--- helloreact/src/App.jsx
+++ helloreact/src/App.jsx
@@ -3,12 +3,16 @@ import './App.css'
 import Cabecalho from './components/Cabecalho'
 
 function App() {
+  function tratarTeste(){
+    console.log("clicou")
+  }
 
   return (
     <>
       <Cabecalho />
       <h1>Hello React</h1>
       <p>paragrafo normal</p>
+      <button onClick={tratarTeste}>Teste</button>
     </>
   )
 }
