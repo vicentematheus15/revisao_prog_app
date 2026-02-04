17695322f07103cfce0b349a916c67b5e334de8d
vicentematheus15 <vicentematheus1507@gmail.com>
2026-02-03 21:04:48 -0300

    refactor: update App component to manage state and handle input changes

--- helloreact/src/App.jsx
+++ helloreact/src/App.jsx
@@ -1,10 +1,13 @@
-
 import './App.css'
 import Cabecalho from './components/Cabecalho'
+import { useState } from 'react'
 
 function App() {
+  const [cont, setCont] = useState(0)
+  const [inputPreco, setInputPreco] = useState('')
+
   function tratarTeste(){
-    console.log("clicou")
+    setCont(cont+1)
   }
 
   return (
@@ -12,7 +15,13 @@ function App() {
       <Cabecalho />
       <h1>Hello React</h1>
       <p>paragrafo normal</p>
+      <input type="text" 
+        value={inputPreco}
+        onChange={(event) => setInputPreco(event.target.value)}
+      />
+      {inputPreco}
       <button onClick={tratarTeste}>Teste</button>
+      {cont}
     </>
   )
 }
