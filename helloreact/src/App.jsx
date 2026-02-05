f982e7c4ba5d29d495f6eacaef30a64c657c02f2
vicentematheus15 <vicentematheus1507@gmail.com>
2026-02-05 20:48:01 -0300

    feat: add ListaProdutos component to display product list

--- helloreact/src/App.jsx
+++ helloreact/src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import Cabecalho from './components/Cabecalho'
 import Imesada from './components/Imesada'
+import ListaProdutos from './components/ListaProdutos'
 
   function App(){
     
     return (
       <div className='conteiner-app'>
       <Cabecalho />
-      
+      <ListaProdutos/>
       {/* <Imesada/> */}
     </div>
   )
---
+++ helloreact/src/components/ListaProdutos.jsx
@@ -0,0 +1,24 @@
+
+function ListaProdutos() {
+
+    const produtos = [
+        {nome: "Garrafa", preco: 50, descricao: "Garrafa do Relâmpago Marquinhos"},
+        {nome: "Teclado", preco: 50, descricao: "Teclado do Relâmpago Marquinhos"}
+    ]
+
+    const produto = {
+        nome: "Garrafa",
+        preco: 50,
+        descricao: "Garrafa de água 1L"
+    }
+
+  return (
+
+    <div>
+        <p>ListaProdutos</p>
+      
+    </div>
+  )
+}
+
+export default ListaProdutos
